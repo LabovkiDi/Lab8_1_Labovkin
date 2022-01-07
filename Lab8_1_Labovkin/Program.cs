b5f82135219a4bf32800e34a2f12a85b6df8d319
Дмитрий Лабовкин <labovkindi@mail.ru>
2022-01-07 17:41:08 +0300

    Добавьте файлы проекта.

---
+++ Lab8_1_Labovkin/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8_1_Labovkin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Admin\Desktop\работа";
+
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+                foreach (var file in directoryInfo.GetFiles())
+                {
+                    Console.WriteLine("Файл: " + file.Name);
+                }
+                foreach (var directory in directoryInfo.GetDirectories())
+                {
+                    Console.WriteLine("Подкаталог: " + directory.Name);
+
+                    foreach (var file in directory.GetFiles())
+                    {
+                        Console.WriteLine("\tФайл: " + file.Name);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Указанный каталог отсутствует");
+            }
+            Console.ReadKey();
+        }
+    }
+}
